cca10a1fa9d14044a92bb11f79ef57dd94577ef2
Razvan Hlihor <razvan.hlihor@gmail.com>
2026-03-16 15:52:36 +0100

    fix: comment out temporary redirects in next.config.ts to allow rendering on usevelocine.com

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,32 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/:path*",
+  //       has: [
+  //         {
+  //           type: "host",
+  //           value: "usevelocine.com",
+  //         },
+  //       ],
+  //       destination: "https://velocine.ai/:path*",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/:path*",
+  //       has: [
+  //         {
+  //           type: "host",
+  //           value: "www.usevelocine.com",
+  //         },
+  //       ],
+  //       destination: "https://velocine.ai/:path*",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
